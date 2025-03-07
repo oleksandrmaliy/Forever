83618d3a1e46856c992cced8d841bfe138b81c78
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-03-07 22:17:53 +0200

    hero only

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import Container from "./components/container";
 import Top from "./components/top.jsx";
 import Header from "./components/header.jsx";
 import HeroBlock from "./components/heroBlock.jsx";
-import ShopBlock from "./components/shopBlock.jsx";
-import ForeverBlock from "./components/foreverBlock.jsx";
+// import ShopBlock from "./components/shopBlock.jsx";
+// import ForeverBlock from "./components/foreverBlock.jsx";
 import Footer from "./components/footer.jsx";
 import Bottom from "./components/bottom.jsx";
-import { ScrollToTopButton } from "./components/upBottom.jsx";
+// import { ScrollToTopButton } from "./components/upBottom.jsx";
 
 // import ScrollAnimation from "./components/xtest.jsx";
 // import ScrollAnimation2 from "./components/xxtest.jsx";
@@ -21,10 +21,10 @@ const App = () => {
       </Top>
       <Container>
         <HeroBlock />
-        <ShopBlock />
+        {/* <ShopBlock /> */}
         {/* <ScrollAnimation /> */}
         {/* <ScrollAnimation2 /> */}
-        <ForeverBlock />
+        {/* <ForeverBlock /> */}
         {/* <div className="h-12 w-12 animate-spin rounded-full border-4 border-blue-500 border-t-transparent"></div>
           <div className="h-10 w-10 animate-ping rounded-full bg-blue-500"></div>
           <div className="flex h-16 w-16 animate-bounce items-center justify-center bg-red-500 text-white">
@@ -37,7 +37,7 @@ const App = () => {
           <Footer />
         </Container>
       </Bottom>
-      <ScrollToTopButton />
+      {/* <ScrollToTopButton /> */}
     </>
   );
 };
--- src/components/heroBlock.jsx
+++ src/components/heroBlock.jsx
@@ -1,31 +1,31 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 import natalya from "../assets/images/Natalya.webp";
 
 const HeroBlock = () => {
-  const [showText, setShowText] = useState(false);
-  const [effect, setEffect] = useState(false);
+  // const [showText, setShowText] = useState(true);
+  // const [effect, setEffect] = useState(false);
 
-  const identity = "hero";
+  // const identity = "hero";
 
-  useEffect(() => {
-    if (effect & !showText) {
-      document.getElementById(identity)?.scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-      });
-    }
-  }, [effect, showText]);
+  // useEffect(() => {
+  //   if (effect & !showText) {
+  //     document.getElementById(identity)?.scrollIntoView({
+  //       behavior: "smooth",
+  //       block: "center",
+  //     });
+  //   }
+  // }, [effect, showText]);
 
-  const handleClick = () => {
-    setShowText(!showText);
-    setEffect(true);
-  };
+  // const handleClick = () => {
+  //   setShowText(!showText);
+  //   setEffect(true);
+  // };
 
-  const scrollToFooter = () => {
-    const footer = document.getElementById("footer");
-    footer?.scrollIntoView({ behavior: "smooth" });
-  };
+  // const scrollToFooter = () => {
+  //   const footer = document.getElementById("footer");
+  //   footer?.scrollIntoView({ behavior: "smooth" });
+  // };
 
   return (
     <div
@@ -36,80 +36,79 @@ const HeroBlock = () => {
         <img
           src={natalya}
           alt="Natalya"
-          className="-mt-10 w-2/3 rounded-full border-8 border-white shadow-all-green sm:-mt-7 md:relative md:-top-12 md:-left-0 md:w-full lg:-top-16 xl:-top-20"
+          className="relative -top-11 w-2/3 rounded-full border-8 border-white shadow-all-green sm:-top-13 md:-top-12 md:w-full lg:-top-16 xl:-top-20"
         />
       </div>
       <div className="col-span-2 mt-4 flex flex-col justify-between md:mt-0">
         <div className="mb-4">
           <div className="mb-4">
-            <p className="mb-4 indent-14">
-              <b> Вітаю вас!</b>
+            <p className="mb-4 indent-14 font-vibes text-5xl">
+              <b>Вітаю Вас!</b>
             </p>
             <p className="indent-14">
               Мене звати Наталя, і я – підприємець компанії Forever. Два роки
               тому я зробила важливий вибір – розпочати власну справу, яка не
               лише приносить дохід, а й наповнює моє життя сенсом та свободою.
             </p>
-            {showText && (
-              <div>
-                <p className="indent-14">
-                  Коли я вперше познайомилася з продукцією Forever, мене вразила
-                  її якість і натуральний склад. Я зрозуміла, що хочу не просто
-                  користуватися нею сама, а й ділитися цими унікальними
-                  продуктами з іншими. Так і почався мій шлях у бізнесі, який
-                  відкрив переді мною безліч можливостей.
-                </p>
-                <p className="indent-14">
-                  Сьогодні я з упевненістю можу сказати, що Forever – це більше,
-                  ніж просто продукція. Це стиль життя, це можливість
-                  піклуватися про своє здоров’я, виглядати чудово та почуватися
-                  енергійною кожного дня. Крім того, це шанс для кожного, хто
-                  хоче змінити своє життя та знайти фінансову незалежність.
-                </p>
-                <p className="indent-14">
-                  Якщо ви хочете спробувати якісну продукцію Forever або
-                  дізнатися більше про можливості, які вона відкриває, буду рада
-                  допомогти вам підібрати ідеальний варіант саме для вас.
-                  Напишіть мені – з радістю поділюся своїм досвідом та підкажу
-                  найкраще рішення для ваших потреб!
-                </p>
-                <br />
-                <p className="mb-4 indent-14">
-                  <b>Вітаю вас!</b>
-                </p>
-                <p className="indent-14">
-                  Мене звати Наталя, і я підприємець компанії Forever. Два роки
-                  тому я вирішила розпочати власну справу, яка дозволяє мені
-                  поєднувати якісні продукти для здоров’я та можливість
-                  розвитку.
-                </p>
-                <p className="indent-14">
-                  Продукція Forever привернула мою увагу своїм натуральним
-                  складом і високими стандартами якості. Я спробувала її сама,
-                  переконалася в результатах і вирішила поділитися цим з іншими.
-                  Так я прийшла до бізнесу, який став для мене не лише роботою,
-                  а й способом зробити щось корисне для людей.
-                </p>
-                <p className="indent-14">
-                  Forever – це можливість підтримувати здоров’я, доглядати за
-                  собою та обирати якісні продукти. Якщо вам цікаво дізнатися
-                  більше або спробувати продукцію, буду рада відповісти на
-                  запитання та допомогти з вибором.
-                </p>
-              </div>
-            )}
+            {/* {showText && ( */}
+            <div>
+              <p className="indent-14">
+                Коли я вперше познайомилася з продукцією Forever, мене вразила
+                її якість і натуральний склад. Я зрозуміла, що хочу не просто
+                користуватися нею сама, а й ділитися цими унікальними продуктами
+                з іншими. Так і почався мій шлях у бізнесі, який відкрив переді
+                мною безліч можливостей.
+              </p>
+              <p className="indent-14">
+                Сьогодні я з упевненістю можу сказати, що Forever – це більше,
+                ніж просто продукція. Це стиль життя, це можливість піклуватися
+                про своє здоров’я, виглядати чудово та почуватися енергійною
+                кожного дня. Крім того, це шанс для кожного, хто хоче змінити
+                своє життя та знайти фінансову незалежність.
+              </p>
+              <p className="indent-14">
+                Якщо ви хочете спробувати якісну продукцію Forever або дізнатися
+                більше про можливості, які вона відкриває, буду рада допомогти
+                вам підібрати ідеальний варіант саме для вас. Напишіть мені – з
+                радістю поділюся своїм досвідом та підкажу найкраще рішення для
+                ваших потреб!
+              </p>
+              <br />
+              <p className="mb-4 indent-14">
+                <b>Вітаю вас!</b>
+              </p>
+              <p className="indent-14">
+                Мене звати Наталя, і я підприємець компанії Forever. Два роки
+                тому я вирішила розпочати власну справу, яка дозволяє мені
+                поєднувати якісні продукти для здоров’я та можливість розвитку.
+              </p>
+              <p className="indent-14">
+                Продукція Forever привернула мою увагу своїм натуральним складом
+                і високими стандартами якості. Я спробувала її сама,
+                переконалася в результатах і вирішила поділитися цим з іншими.
+                Так я прийшла до бізнесу, який став для мене не лише роботою, а
+                й способом зробити щось корисне для людей.
+              </p>
+              <p className="indent-14">
+                Forever – це можливість підтримувати здоров’я, доглядати за
+                собою та обирати якісні продукти. Якщо вам цікаво дізнатися
+                більше або спробувати продукцію, буду рада відповісти на
+                запитання та допомогти з вибором.
+              </p>
+            </div>
+            {/* )} */}
           </div>
 
-          <div className="flex justify-center">
+          {/* <div className="flex justify-center">
             <button
               onClick={handleClick}
               className="cursor-pointer text-greencolor"
             >
               {showText ? "Згорнути" : "Читати більше..."}
             </button>
-          </div>
+          </div> */}
         </div>
-        <div className="mt-auto">
+        {/* <div className="mt-auto">
           <button
             onClick={scrollToFooter}
             className="flex w-full items-center bg-greencolor/90 p-4 hover:bg-redcolor"
@@ -118,7 +117,7 @@ const HeroBlock = () => {
               Зв&apos;яжіться зі мною
             </p>
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
